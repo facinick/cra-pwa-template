b4c3ffbe1b2173c9c6df4d8b2211fc3923609d8d
xaovnumwsercz <facinick@gmail.com>
2021-09-07 21:52:04 +0530

    added react fire

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,17 +8,23 @@ import { observer } from 'mobx-react-lite';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { useThemeStore } from '../stores/provider';
 import { HelloWorld } from './HelloWorld';
+import { FirebaseAppProvider } from 'reactfire';
+// import { getFirestore } from 'firebase/firestore';
+import { firebaseConfig } from '../private/firebase_config';
 
 const App = observer(function App() {
     const { theme } = useThemeStore();
+    // const firestoreInstance = getFirestore(useFirebaseApp());
 
     return (
-        <ThemeProvider theme={theme}>
-            <div className="App">
-                <CssBaseline />
-                <HelloWorld />
-            </div>
-        </ThemeProvider>
+        <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+            <ThemeProvider theme={theme}>
+                <div className="App">
+                    <CssBaseline />
+                    <HelloWorld />
+                </div>
+            </ThemeProvider>
+        </FirebaseAppProvider>
     );
 });
 
